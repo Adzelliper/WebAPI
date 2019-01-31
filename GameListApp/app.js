200c0f96fcb555fe338126022cdf0bb0b65cc2bd
Perez <rperez2@labs.neit.edu>
2019-01-31 14:57:44 -0500

    delete buttons

--- GameListApp/app.js
+++ GameListApp/app.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var exphbs = require('express-handlebars');
 var app = express();
 var port = 5000;
 var path = require('path');
@@ -6,61 +7,86 @@ var router = express.Router();
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 
-//gets rid of warning for Mongoose
+
 mongoose.Promise = global.Promise;
 
-//connect to mongodb using mongoose
+//connect to mongodb using mongoose 
 mongoose.connect("mongodb://localhost:27017/gameentries", {
     useMongoClient:true
-})
-.then(function(){console.log("MongoDB Connected")})
-.catch(function(err){console.log(err)});
+}).then(function(){
+    console.log("Connected to the Monogo Database")
+}).catch(function(err){
+    console.log(err);
+});
 
-//Load in Entry Model
 require('./models/Entry');
 var Entry = mongoose.model('Entries');
 
-//funcitons needed to run body-parser
+app.engine('handlebars', exphbs({
+    defaultLayout:'main'
+}));
+app.set('view engine', 'handlebars');
+
+// functions to use body parser 
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 
-//Route to index.html
-router.get('/', function(req, res){
-    res.sendFile(path.join(__dirname+'/index.html'));
+//route to index.html
+router.get('/',function(req, res){
+    //res.sendFile(path.join(__dirname+'/index.html'));
+    //var title = "Welcome to the GameApp";
+    res.render('index');
 });
 
-//Route to entries.html
-router.get('/entries', function(req, res){
-    res.sendFile(path.join(__dirname+'/entries.html'));
+//Route to entries
+router.get('/entries',function(req, res){
+    res.render('gameentries/addgame');
 });
 
-app.get('/getdata', function(req, res){
-    console.log("request made from fetch");
-    Entry.find({})
-    .then(function(entries){
-        res.send({
-            entries:entries
-        });
+//Route to login
+router.get('/login',function(req, res){
+    res.render('login');
+});
+
+app.get('/', function(req,res){
+    console.log("Request made from fetch");
+    Entry.find({}).then(function(entries){
+        res.render("index", {entries:entries})
     });
 });
 
-//post from foorm on index.html
-app.post('/', function(req, res){
+//route to entries.html
+router.get('/entries',function(req, res){
+    res.sendFile(path.join(__dirname+'/entries.html'));
+});
+
+//post for form on index.html
+app.post('/addgame', function(req,res){
     console.log(req.body);
     var newEntry = {
         title:req.body.title,
         genre:req.body.genre
     }
+
     new Entry(newEntry).save().then(function(entry){
-        res.redirect('/')});
+        res.redirect('/');
+    });
 });
 
+//Delete Game Entry
+app.post('/:id', function(req,res){
+    Entry.remove({_id:req.params.id}).then(function(){
+        //req.flash("game removed");
+        res.redirect('/');
+    });
+});
 
 //routs for paths
-app.use(express.static(__dirname+'/views'));
-app.use(express.static(__dirname+'/scripts'));
+app.use(express.static(__dirname + '/views'));
+app.use(express.static(__dirname + '/scripts'))
 app.use('/', router);
-//starts server
+
+//starts the server 
 app.listen(port, function(){
-    console.log("Server is running on port 5000");
+    console.log("server is running on port: " + port);
 });
